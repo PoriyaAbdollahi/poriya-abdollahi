46993b9c631f4994e5b02f5a1395f05908abb3d7
PoriyaAbdollahi <poriyaa58@gmail.com>
2022-01-07 04:39:39 -0800

    adding animation and style to navlinks

--- src/Components/Navigation/Navigation.tsx
+++ src/Components/Navigation/Navigation.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import "./Navigation.css"
-
+const Span = ()=>(<div><span></span><span></span><span></span><span></span></div>)
 const Navigation = () => {
     return (
-        <nav class = "topnav" >
-            <div style={{float:"right" }}>
-            <Link to = "/">{`//01. <خانه/>` }</Link>
-            <Link to = "/">{`//02. <نمونه کارها/>` }</Link>
+        <nav  >
+            <div style={{float:"right" ,margin:'42px'}}>
+            <Link className= {'btn'} to = "/"><Span/>{`//01. <خانه/>` }</Link>
+            <Link className= {'btn'} to = "/"><Span/>{`//02. <نمونه کارها/>` }</Link>
             </div>
-            <div style={{float:"left"}}>
-            <Link to = "/">{`//03. <تماس با من/>` }</Link>
-            <Link to = "/">{`//04. <مطبوعات/>` }</Link>
+            <div style={{float:"left"  ,margin:'42px' }}>
+            <Link className= {'btn'} to = "/"><Span/>{`//03. <تماس با من/>` }</Link>
+            <Link className= {'btn'} to = "/"><Span/>{`//04. <مطبوعات/>` }</Link>
             </div>
             
         </nav>
