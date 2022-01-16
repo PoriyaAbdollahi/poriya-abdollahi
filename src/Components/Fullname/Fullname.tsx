5f92718827ee707250ea3c0e9ddbf65cda45a6d1
PoriyaAbdollahi <poriyaa58@gmail.com>
2022-01-16 06:03:02 -0800

    resume btn fixed

--- src/Components/Fullname/Fullname.tsx
+++ src/Components/Fullname/Fullname.tsx
@@ -4,6 +4,7 @@ import SectionWrapper from '../SectionWrapper/SectionWrapper'
 const Fullname = () => {
     return (
         <SectionWrapper>
+            <div className='flex-container flex-item-left'>
             <div className="who-i-am">
                 <p>{`<p>`}<span>من</span>{`</p>`}</p>
                 <span>{"<h1>"}</span>
@@ -11,6 +12,14 @@ const Fullname = () => {
                 <span>{"</h1>"}</span>
                 <p>{`<p>`}<span>هستم</span>{`</p>`}</p>
                 <p className='fullname__describe'>{`<p>`}<span>برنامه نویس اندروید و فرانت اند</span>{`</p>`}</p>
+             </div>
+             <div className='curriculumvitae flex-item-right'>   
+             <div className='circleholder' id='resumebtn'>
+                <div className="outerCircle"></div>
+                <div className="innerCircle"></div>
+                <div className="icon"> دانلود رزومه </div>
+            </div>              
+             </div>
              </div>
         </SectionWrapper>
     )
