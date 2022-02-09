907bd9333586efcdb5cb9f57d90379d1a6129381
PoriyaAbdollahi <poriyaa58@gmail.com>
2022-02-10 00:45:45 +0330

    adding content to the About me section

--- src/Components/AboutMe/AboutMe.tsx
+++ src/Components/AboutMe/AboutMe.tsx
@@ -19,7 +19,13 @@ const AboutMe = () => {
        <h2 className='about__me__title'><span>{`<h2>`}</span>درباره من<span>{`</h2>`}</span></h2>
       <div className='about__me'>
       <Img className={'about__me__image'} fluid={data.file.childImageSharp.fluid} alt="عکس پوریا عبدالهی"/>
-      <p> yek do yek do yek do yek do yek do yek do yek do yek do yek do yek do yek do yek do yek do yek do yek do yek do yek do yek do yek do yek do do yek do yek do yek do do yek do yek do yek do do yek do yek do yek do </p>
+      <p><span>{`<p>`}</span>من برنامه نویس و توسعه دهنده هستم همینطور دانشجوی مهندسی نرم افزار 
+ ۳ سال اخیر زندگیمو صرف مطالعه و روی نرم افزار ها و یادگیری زبان های مختلف
+برنامه نویسی کردم
+حوزه هایی که بیشتر در اون تمرکز داشتم  برنامه نویسی اندروید و  فراند اند 
+و همینطور کامپایلر ها بوده  
+۲۲ سالمه و از ۱۲ سالگی کار کردن با کامپیوتر رو یاد گرفتم 
+<span>{`</p>`}</span></p>
       </div>
   </SectionWrapper>);
 };
