bb3036677c7a65e7ec3d8debdecd4e53896ef8f0
PoriyaAbdollahi <poriyaa58@gmail.com>
2022-03-03 17:40:41 +0330

    portfolio image content added and title for the projects

--- src/static/staticData.ts
+++ src/static/staticData.ts
@@ -6,31 +6,31 @@
         {
             title:"FCSHOP",
             description:`یک  ‍ پروژه فروشگاهی است که قسمت فرانت اند آن را طراحی و برنامه نویسی کردم از کتابخانه های مختلفی استفاده شده `,
-            img:"https://www.outbrain.com/techblog/wp-content/uploads/2017/05/road-sign-361513_960_720.jpg",
+            img:"FCSHOP.png",
             tag:[{name:"React" , color :"blue" },{name:"Redux" , color :"purple" },{name:"Bootstrap" , color :"purple" }]
         },
         {
             title:"کابین",
             description:" کابین یک پروژه بازی  حدس کلمات  برای اندروید است تکنلوژی های که در آن استفاده کردم جاوا در اون از الگوریتم رمز گذاری استفاده شده همچنین  مراحل بازی برای هر گوشی متفاوت است نسبت به دیگران ",
-            img:"https://www.outbrain.com/techblog/wp-content/uploads/2017/05/road-sign-361513_960_720.jpg"
+            img:"cabin2.png"
             ,tag:[{name:"Android Native" , color :"green" },{name:"Java" , color :"blue" },{name:"Sharedprefrence" , color :"red" }]
         },
         {
             title:"HSQL",
             description:" یک زبان است متن باز است نوشتن گرامر و طراحی زبان  طراحی سایت در این پروژه توسط من انجام شده در اون از تایپ اسکریپت استفاده شده",
-            img:"https://www.outbrain.com/techblog/wp-content/uploads/2017/05/road-sign-361513_960_720.jpg"
+            img:"hsql.png"
             ,tag:[{name:"antlr4" , color :"pink" },{name:"typescript" , color :"blue" },{name:"CI/CD" , color :"purple" },{name:"gatsby" , color :"pink" },{name:"React" , color :"blue" }]
         },
         {
             title:"Annotion App (Uncle horse)",
             description:"یک وب اپ هست که کاربر اجازه  میده اطراف  شی ها داخل عکس رو مشخص کنه و دیتا خودش رو برای هوش مصنوعی آماده کند ",
-            img:"https://www.outbrain.com/techblog/wp-content/uploads/2017/05/road-sign-361513_960_720.jpg"
+            img:"unclehorse.png"
             ,tag:[{name:"javascript" , color :"yellow" },{name:"farbic.js" , color :"orange" }]
         },
         {
             title:"Mahzyar",
             description:"اپ اندروید شرکت مهزیار فولاد ",
-            img:"https://www.outbrain.com/techblog/wp-content/uploads/2017/05/road-sign-361513_960_720.jpg"
+            img:"mahyzar.png"
             ,tag:[{name:"Android Native" , color :"green" },{name:"java" , color :"blue" }]
         }
     ]
