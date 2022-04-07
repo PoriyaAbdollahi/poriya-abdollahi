211496066c07af251d6d29452c28c45937b57298
PoriyaAbdollahi <poriyaa58@gmail.com>
2022-04-07 23:26:51 +0430

    adding static component

--- src/Components/AboutMe/AboutMe.tsx
+++ src/Components/AboutMe/AboutMe.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './AboutMe.css'
 import SectionWrapper from '../SectionWrapper/SectionWrapper';
-
+import { StaticImage } from 'gatsby-plugin-image';
 const AboutMe = () => {
 
   return (
   <SectionWrapper>
       <h2 id="aboutme" className='about__me__title'><span>{`<h2>`}</span>درباره من<span>{`</h2>`}</span></h2>
       <div className='about__me'>
-      <img className={'about__me__image'} src={"../../../static/thisisme.png"} alt="عکس پوریا عبدالهی"/>
+      <StaticImage className={'about__me__image'} src={"../../../static/thisisme.png"} alt="عکس پوریا عبدالهی"/>
       <p><span>{`<p>`}</span>من برنامه نویس و توسعه دهنده هستم همینطور دانشجوی مهندسی نرم افزار 
  ۳ سال اخیر زندگیمو صرف مطالعه و روی نرم افزار ها و یادگیری زبان های مختلف
 برنامه نویسی کردم
