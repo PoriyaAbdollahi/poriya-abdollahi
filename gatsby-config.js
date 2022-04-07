d0e4548cdef960975c1ceb752a7f119a1d000d40
PoriyaAbdollahi <poriyaa58@gmail.com>
2022-04-07 23:02:10 +0430

    pathprefix and site info updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Poriya Abdollahi`,
+    description: `poriya abdollahi personal website`,
+    author: `Poriya abdollahi`,
+    siteUrl: `https://poriyaabdollahi.github.io/`,
   },
+  pathPrefix: `poriya-abdollahi`,
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
