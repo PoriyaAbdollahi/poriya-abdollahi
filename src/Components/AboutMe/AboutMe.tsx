24680372fb9b34bcaab505976c328788f3c616c8
PoriyaAbdollahi <poriyaa58@gmail.com>
2022-04-07 22:31:42 +0430

    fixing issue with loading about me image

--- src/Components/AboutMe/AboutMe.tsx
+++ src/Components/AboutMe/AboutMe.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
 import './AboutMe.css'
 import SectionWrapper from '../SectionWrapper/SectionWrapper';
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
 
 const AboutMe = () => {
-    const data = useStaticQuery(graphql`
-    query {
-      file(relativePath: { eq: "Images/thisisme.png" }) {
-        childImageSharp {
-          fluid {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-    }
-  `)
-  return (<SectionWrapper>
-       <h2 id="aboutme" className='about__me__title'><span>{`<h2>`}</span>درباره من<span>{`</h2>`}</span></h2>
+
+  return (
+  <SectionWrapper>
+      <h2 id="aboutme" className='about__me__title'><span>{`<h2>`}</span>درباره من<span>{`</h2>`}</span></h2>
       <div className='about__me'>
-      <Img className={'about__me__image'} fluid={data.file.childImageSharp.fluid} alt="عکس پوریا عبدالهی"/>
+      <img className={'about__me__image'} src={"../../../static/thisisme.png"} alt="عکس پوریا عبدالهی"/>
       <p><span>{`<p>`}</span>من برنامه نویس و توسعه دهنده هستم همینطور دانشجوی مهندسی نرم افزار 
  ۳ سال اخیر زندگیمو صرف مطالعه و روی نرم افزار ها و یادگیری زبان های مختلف
 برنامه نویسی کردم
