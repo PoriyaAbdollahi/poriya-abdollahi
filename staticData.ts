37d802f2d3391e09133632b23fcdc9acfc7e05e0
PoriyaAbdollahi <PoriyaAbdollahi@users.noreply.github.com>
2022-07-11 16:06:59 +0000

    deployed via Gatsby Publish Action 🎩 for 8142835316e965357381a5592d47dc59359ad8c8

---
+++ staticData.ts
@@ -0,0 +1,38 @@
+ const portfolio = {
+    siteLanguage: {lang:"fa"},
+    fullname: "پوریا عبدالهی روشن",
+    SiteDescription : "سایت شخصی پوریا عبدالهی  ",
+    projects:[
+        {
+            title:"FCSHOP",
+            description:`یک  ‍ پروژه فروشگاهی است که قسمت فرانت اند آن را طراحی و برنامه نویسی کردم از کتابخانه های مختلفی استفاده شده `,
+            img:"FCSHOP.png",
+            tag:[{name:"React" , color :"blue" },{name:"Redux" , color :"purple" },{name:"Bootstrap" , color :"purple" }]
+        },
+        {
+            title:"کابین",
+            description:" کابین یک پروژه بازی  حدس کلمات  برای اندروید است تکنولوژی های که در آن استفاده کردم جاوا در اون از الگوریتم رمز گذاری استفاده شده همچنین  مراحل بازی برای هر گوشی متفاوت است نسبت به دیگران ",
+            img:"cabin2.png"
+            ,tag:[{name:"Android Native" , color :"green" },{name:"Java" , color :"blue" },{name:"Sharedprefrence" , color :"red" }]
+        },
+        {
+            title:"HSQL",
+            description:" یک زبان است متن باز است نوشتن گرامر و طراحی زبان  طراحی سایت در این پروژه توسط من انجام شده در اون از تایپ اسکریپت استفاده شده",
+            img:"hsql.png"
+            ,tag:[{name:"antlr4" , color :"pink" },{name:"typescript" , color :"blue" },{name:"CI/CD" , color :"purple" },{name:"gatsby" , color :"pink" },{name:"React" , color :"blue" }]
+        },
+        {
+            title:"Annotion App (Uncle horse)",
+            description:"یک وب اپ هست که کاربر اجازه  میده اطراف  شی ها داخل عکس رو مشخص کنه و دیتا خودش رو برای هوش مصنوعی آماده کند ",
+            img:"unclehorse.png"
+            ,tag:[{name:"javascript" , color :"yellow" },{name:"farbic.js" , color :"orange" }]
+        },
+        {
+            title:"Mahzyar",
+            description:"اپ اندروید شرکت مهزیار فولاد ",
+            img:"mahyzar.png"
+            ,tag:[{name:"Android Native" , color :"green" },{name:"java" , color :"blue" }]
+        }
+    ]
+}
+export default portfolio
